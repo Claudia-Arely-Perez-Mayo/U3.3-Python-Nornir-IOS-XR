224e7535db9fdb0e1d97bfad139f57bbfc7a7b00
Giobanni-sdc <llomami3@gmail.com>
2021-08-15 21:12:53 -0500

    Update IOS-XRv.py

--- IOS-XRv.py
+++ IOS-XRv.py
@@ -7,7 +7,7 @@ nr = InitNornir(
 )
 
 s1=nr.run(netmiko_send_config, config_file= "IOSXRv.txt")
-s2=nr.run(netmiko_send_config, config_commands=["interface lo 7", "description Equipo2NornirConPython","ipv4 add 7.7.7.7 255.255.255.255", "commit"])
+s2=nr.run(netmiko_send_config, config_commands=["interface lo 7", "description Equipo1NornirConPython","ipv4 add 7.7.7.7 255.255.255.255", "commit"])
 s3=nr.run(netmiko_send_command, command_string="do show ip int brief")
 print_result(s1)
 print_result(s2)
